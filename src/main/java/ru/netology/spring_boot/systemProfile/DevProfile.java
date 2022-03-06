fcb0848cdbbc50b09cf1786257e6025fd34e2115
Aleksey Falev <aleksej.falev@mail.ru>
2022-03-06 20:45:49 +0300

    Initial commit

---
+++ src/main/java/ru/netology/spring_boot/systemProfile/DevProfile.java
@@ -0,0 +1,9 @@
+package ru.netology.spring_boot.systemProfile;
+
+public class DevProfile implements SystemProfile {
+
+    @Override
+    public String getProfile() {
+        return "Current profile is dev";
+    }
+}
